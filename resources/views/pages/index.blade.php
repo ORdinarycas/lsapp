d4c16ed46b39690b3083126c53e500d968c307c5
Cai Ci-Jhan <xcqvmywoj@yahoo.com.tw>
2017-10-04 18:46:22 +0800

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+    <p>
+        <a class="btn btn-primary btn-lg" href="/login" role="button">login</a>
+        <a class="btn btn-success btn-lg" href="/Register" role="button">Register</a>
+    </p>
+</div>
+@endsection
